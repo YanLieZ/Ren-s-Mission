7c089fb3da1def3bc68ec9ac0dc7f0e3469cc0ee
Yabely Gutierrez <yanlie.arts@gmail.com>
2025-09-19 15:54:08 -0500

    actualización realizadaaa

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,35 @@ namespace pan
     {
         static void Main(string[] args)
         {
+            void objetos()
+            {
+                List<items> lista = new List<items>();
 
+                lista.Add(new items("reloj cucú", 23));
+                lista.Add(new items("ouija", 40));
+                foreach (var items in lista) 
+                {
+                    Console.WriteLine(items);
+                }
+            }
+
+            Console.WriteLine("cuanto posees?");
+            float dinero = float.Parse(Console.ReadLine());
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("1.comprar algo?");
+                Console.WriteLine("2.finalizar compra");
+                float respuesta = float.Parse(Console.ReadLine());
+                switch (respuesta)
+                {
+                    case 1:
+                       objetos();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
---
+++ items.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pan
+{
+    internal class items
+    {
+        private string nombreDelObjecto;
+        private float precio;
+
+        public items (string nombreDelObjecto, float precio)
+        {
+            this.nombreDelObjecto = nombreDelObjecto;
+            this.precio = precio;
+        }
+
+        string nombreItem()
+        {
+            return nombreDelObjecto;
+        }
+    }
+}
